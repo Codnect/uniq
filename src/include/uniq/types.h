c5c84fe45674a29d1a9ae8ca0bf15a1ded135ac5
Burak Köken <colcsky@gmail.com>
2014-07-09 02:27:28 +0300

    Create types.h

---
+++ src/include/uniq/types.h
@@ -0,0 +1,66 @@
+/*
+ *  Copyright(C) 2014 Codnect Team
+ *  Copyright(C) 2014 Burak Köken
+ *
+ *  This file is part of Uniq.
+ *  
+ *  Uniq is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2 of the License.
+ *
+ *  Uniq is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UNIQ_TYPES_H__
+#define __UNIQ_TYPES_H__
+
+/*
+ * temel veri tipleri
+ */
+typedef signed char 		s8;
+typedef unsigned char 		u8;
+
+typedef signed short	 	s16;
+typedef unsigned short		u16;
+
+typedef signed int 		s32;
+typedef unsigned int 		u32;
+
+typedef signed char 		int8_t;
+typedef unsigned char 		uint8_t;
+
+typedef signed short 		int16_t;
+typedef unsigned short 		uint16_t;
+
+typedef signed int 		int32_t;
+typedef unsigned int 		uint32_t;
+
+
+/*
+ * mimari veri tipleri
+ */
+typedef signed char 		sbyte_t;
+typedef unsigned char 		byte_t;
+
+typedef signed short 		sword_t;
+typedef unsigned short 		word_t;
+
+typedef signed int 		sdword_t;
+typedef unsigned int 		dword_t;
+
+
+/*
+ * diger ozellesitirilmis veri tipleri
+ */
+typedef unsigned short		port_t;
+
+typedef signed int 		intptr_t;
+typedef unsigned int 		uintptr_t;
+
+#endif /* __UNIQ_TYPES_H__ */
