419cd9dbf009a2c949b24daac078448c1e34c008
Burak Köken <colcsky@gmail.com>
2014-07-13 00:55:55 +0300

    Update string.h

--- src/include/string.h
+++ src/include/string.h
@@ -20,5 +20,25 @@
 #ifndef __UNIQ_STRING_H__
 #define __UNIQ_STRING_H__
 
+extern size_t strlen(const char *s);
+extern size_t strnlen(const char *s,size_t count);
+extern int strcmp(const char *s1,const char *s2);
+extern int strncmp(const char *s1,const char *s2,size_t count);
+extern int strcasecmp(const char *s1,const char *s2);
+extern int strncasecmp(const char *s1,const char *s2,size_t count);
+extern char* strtoupper(char* s);
+extern char* strtolower(char *s);
+extern char* strntoupper(char* s,size_t count);
+extern char* strntolower(char *s,size_t count);
+extern char* strcpy(char *dest, const char *src);
+extern char *strncpy(char *dest, const char *src, size_t count);
+extern int strtobool(const char *s, bool *r);
+extern char *str_ltrim(char *s);
+extern char *str_rtrim(char *s);
+extern char *strim(char *s);
+extern char *strchr(const char *s, int c);
+extern char *strrchr(const char *s, int c);
+
+#define strlchr(s,c)	strchr(s,c)
 
 #endif /* __UNIQ_STRING_H__ */
