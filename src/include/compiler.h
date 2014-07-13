127e9752e6a2d326e14ce6a7dbe6973000dd8532
Burak Köken <colcsky@gmail.com>
2014-07-14 00:07:13 +0300

    Create compiler.h

---
+++ src/include/compiler.h
@@ -0,0 +1,28 @@
+/*
+ *  Copyright(C) 2014 Codnect Team
+ *  Copyright(C) 2014 Burak Köken
+ *
+ *  This file is part of Uniq.
+ *  
+ *  Uniq is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2 of the License.
+ *
+ *  Uniq is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UNIQ_COMPILER_H__
+#define __UNIQ_COMPILER_H__
+
+/* eger gnu c derleyicisi tanimliysa */
+#if defined(__GNUC__)
+	#include <compiler_gcc.h>
+#endif
+
+#endif /* __UNIQ_COMPILER_H__ */
