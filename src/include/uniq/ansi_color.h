0044dec8b8f74d6afdbd89fef4e06b563415694e
Burak Köken <colcsky@gmail.com>
2014-07-18 15:17:03 +0300

    Update ansi_color.h

--- src/include/uniq/ansi_color.h
+++ src/include/uniq/ansi_color.h
@@ -4,11 +4,11 @@
  *
  *  This file is part of Nectlib.
  *  
- *  Nectlib is free software: you can redistribute it and/or modify it under the
+ *  Uniq is free software: you can redistribute it and/or modify it under the
  *  terms of the GNU General Public License as published by the Free Software
  *  Foundation, version 2 of the License.
  *
- *  Nectlib is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  Uniq is distributed in the hope that it will be useful, but WITHOUT ANY
  *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  *  details.
