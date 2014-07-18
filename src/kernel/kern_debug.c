30607b10fd36b475bda74690f7cf725d613b9135
Burak Köken <colcsky@gmail.com>
2014-07-18 21:28:28 +0300

    Update kern_debug.c

--- src/kernel/kern_debug.c
+++ src/kernel/kern_debug.c
@@ -17,3 +17,41 @@
  *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
  */
  
+#include <uniq/kern_debug.h>
+#include <uniq/kernel.h>
+#include <va_list.h>
+
+#ifdef KDEBUG_DEFAULT
+	kern_debug_mode_t kdebug_mode = KERN_DEBUG_DEFAULT;
+#else  /* KDEBUG_OOPS */
+	kern_debug_mode_t kdebug_mode = KERN_DEBUG_OOPS;
+#endif
+
+static char *level_msgs[]={
+	"\033[1;32mINFO\033[0m",
+	"\033[1;37mNOTICE\033[0m",
+	"\033[1;33mWARNING\033[0m",
+	"\033[1;31mERROR\033[0m",
+	"\033[1;34mCRITICAL\033[0m",
+	"\033[1;31mALERT\033[0m",
+	"\033[1;36mEMERGENCY\033[0m"
+};
+
+void _debug_print(char *file,int line,kern_levels_t level,const char *fmt,...){
+	
+	if((level == KERN_INFO) && (kdebug_mode == KERN_DEBUG_OOPS))
+		return;
+
+	char buffer[1024];
+	va_list arg_list;
+	va_start(arg_list,fmt);
+	vsnprintf(buffer,sizeof(buffer)-1,fmt,arg_list);
+	va_end(arg_list);
+	
+	char *lvl_text;
+	
+	lvl_text = level_msgs[level];
+	printf("%-10s : [module = %s line = %d]\n %10s %s\n",lvl_text,file,line,"->",buffer);
+	
+}
+
