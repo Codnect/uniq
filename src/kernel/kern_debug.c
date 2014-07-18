7cabb95cdbf8a5a6573b1ed2ad6f60a9e9faea87
Burak Köken <colcsky@gmail.com>
2014-07-18 23:19:52 +0300

    Update kern_debug.c

--- src/kernel/kern_debug.c
+++ src/kernel/kern_debug.c
@@ -64,7 +64,7 @@ void _debug_print(char *file,int line,kern_levels_t level,const char *fmt,...){
 	char *lvl_text;
 	
 	lvl_text = level_msgs[level];
-	printf("%s : [module = %s line = %d]\n\t%s %s\n",lvl_text,file,line,"->",buffer);
+	printf("%-10s: [module = %s line = %d]\n%s %s\n",lvl_text,file,line,"->",buffer);
 	
 }
 
