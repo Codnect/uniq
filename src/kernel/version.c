7e1eed60e74bc1f95b47297a0b8dc4f9f9c39d65
Burak Köken <colcsky@gmail.com>
2014-07-19 13:57:21 +0300

    Update version.c

--- src/kernel/version.c
+++ src/kernel/version.c
@@ -17,3 +17,33 @@
  *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <uniq/version.h>
+
+char * __kern_name = "uniq";
+char * __kern_version_format = "%d.%d.%d-%s";
+
+uint8_t __kern_version_major = 0;
+uint8_t __kern_version_minor = 0;
+uint8_t __kern_version_lower = 1;
+
+#ifdef KERN_SPEC_SUFFIX
+	#define STR(x) #x
+	#define STRSTR(x) STR(x)
+	#define KERN_VERSION_SUFFIX STRSTR(KERN_SPEC_SUFFIX)	/* ozel ekler icin */
+#else
+	#define KERN_VERSION_SUFFIX "beta"			/* beta,final */
+#endif
+char * __kern_version_suffix   = KERN_VERSION_SUFFIX;
+char * __kern_version_codename = "nova";
+
+char * __kern_arch = "i386";
+char * __kern_build_date = __DATE__;
+char * __kern_build_time = __TIME__;
+
+#if (defined(__GNUC__) || defined(__GNUG__)) && !(defined(__clang__) || defined(__INTEL_COMPILER))
+# define COMPILER_VERSION "gcc " __VERSION__
+#else
+# define COMPILER_VERSION "unknown compiler"
+#endif
+
+char * __kern_compiler_version = COMPILER_VERSION;
