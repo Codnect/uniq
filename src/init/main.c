303eec6a04a10b0fb4512768f338dda3f800e4e0
Burak Köken <colcsky@gmail.com>
2014-07-19 18:56:08 +0300

    Update main.c

--- src/init/main.c
+++ src/init/main.c
@@ -26,8 +26,8 @@ void kmain(uintptr_t mboot_magic){
 	init_vga_console();
 
 	if(mboot_magic != 0x2badb002)
-		debug_print(KERN_WARNING,"invalid magic number. magic number = \033[1;31m%P",mboot_magic);
+		debug_print(KERN_WARNING,"Invalid the magic number. Magic number = \033[1;31m%P",mboot_magic);
 	else
-		debug_print(KERN_INFO,"magic number was checked. magic number = \033[1;37m%P",mboot_magic);
+		debug_print(KERN_INFO,"Valid the magic number. Magic number = \033[1;37m%P",mboot_magic);
 
 }
