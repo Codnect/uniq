4e0317e2c593b5755cbc291224088d48994a8997
Burak Köken <colcsky@gmail.com>
2014-07-26 23:18:25 +0300

    Update idt.c

--- src/arch/i386/cpu/idt.c
+++ src/arch/i386/cpu/idt.c
@@ -18,6 +18,21 @@
  */
 
 #include <uniq/module.h>
+#include <uniq/types.h>
+
+struct idt_entry_t {
+	uint16_t base_low;
+	uint16_t sel;
+	uint8_t zero;
+	uint8_t flags;
+	uint16_t base_high;
+} __attribute__((packed));
+
+
+struct idt_ptr_t {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
 
 /*
  * init_idt
