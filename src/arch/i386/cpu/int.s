d677f984ccdce7d74698ba983686bf926fb9ff70
Burak Köken <colcsky@gmail.com>
2014-07-29 15:12:56 +0300

    Update int.s

--- src/arch/i386/cpu/int.s
+++ src/arch/i386/cpu/int.s
@@ -22,7 +22,7 @@ BITS 32
 
 ; isr ve irq makrolari
 ; tum isr ve irq'lara ayri ayri kod yazicagimiza isimizi
-; makrolarlar halledelim ;)
+; makrolarla halledelim ;)
 
 ; hata kodu icermeyen istisna kesmeleri icin macro
 %macro ISR_NOERR 1			; 1 sayisi 1 adet parametre aldigini gosteriyor
@@ -56,6 +56,62 @@ BITS 32
 		push byte %2		; %2, 2.parametre. kesme numarasini sakla
 		jmp irq_common_entry    ; irq'lar icin ortak girise zipla
 %endmacro
+
+
+; standart x86 kesme servis rutinleri
+ISR_NOERR 0
+ISR_NOERR 1
+ISR_NOERR 2
+ISR_NOERR 3
+ISR_NOERR 4
+ISR_NOERR 5
+ISR_NOERR 6
+ISR_NOERR 7
+ISR_ERR   8
+ISR_NOERR 9
+ISR_ERR   10
+ISR_ERR   11
+ISR_ERR   12
+ISR_ERR   13
+ISR_ERR   14
+ISR_NOERR 15
+ISR_NOERR 16
+ISR_NOERR 17
+ISR_NOERR 18
+ISR_NOERR 19
+ISR_NOERR 20
+ISR_NOERR 21
+ISR_NOERR 22
+ISR_NOERR 23
+ISR_NOERR 24
+ISR_NOERR 25
+ISR_NOERR 26
+ISR_NOERR 27
+ISR_NOERR 28
+ISR_NOERR 29
+ISR_NOERR 30
+ISR_NOERR 31
+ISR_NOERR 127
+
+
+; irq
+IRQ_ENTRY 0, 32
+IRQ_ENTRY 1, 33
+IRQ_ENTRY 2, 34
+IRQ_ENTRY 3, 35
+IRQ_ENTRY 4, 36
+IRQ_ENTRY 5, 37
+IRQ_ENTRY 6, 38
+IRQ_ENTRY 7, 39
+IRQ_ENTRY 8, 40
+IRQ_ENTRY 9, 41
+IRQ_ENTRY 10, 42
+IRQ_ENTRY 11, 43
+IRQ_ENTRY 12, 44
+IRQ_ENTRY 13, 45
+IRQ_ENTRY 14, 46
+IRQ_ENTRY 15, 47
+
 
 extern fault_handler
 
