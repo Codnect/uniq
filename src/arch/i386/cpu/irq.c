092db33351aa5086de15fbb2cac5af8268b71fee
Burak Köken <colcsky@gmail.com>
2014-07-29 16:12:35 +0300

    Update irq.c

--- src/arch/i386/cpu/irq.c
+++ src/arch/i386/cpu/irq.c
@@ -21,6 +21,27 @@
 #include <uniq/kernel.h>
 
 /*
+ * irq isleyicileri
+ */
+extern void _irq0(void);
+extern void _irq1(void);
+extern void _irq2(void);
+extern void _irq3(void);
+extern void _irq4(void);
+extern void _irq5(void);
+extern void _irq6(void);
+extern void _irq7(void);
+extern void _irq8(void);
+extern void _irq9(void);
+extern void _irq10(void);
+extern void _irq11(void);
+extern void _irq12(void);
+extern void _irq13(void);
+extern void _irq14(void);
+extern void _irq15(void);
+
+
+/*
  * irq_set_gates
  */
 void irq_set_gates(void){
