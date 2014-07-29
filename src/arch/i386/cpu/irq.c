11d6477f3f07173344b7d1464ede29f4ccf99578
Burak Köken <colcsky@gmail.com>
2014-07-29 23:40:28 +0300

    Update irq.c

--- src/arch/i386/cpu/irq.c
+++ src/arch/i386/cpu/irq.c
@@ -18,8 +18,8 @@
  */
  
 #include <uniq/module.h>
-#include <uniq/types.h>
 #include <uniq/regs.h>
+#include <uniq/kernel.h>
 
 /*
  * irq isleyicileri
@@ -42,6 +42,11 @@ extern void _irq14(void);
 extern void _irq15(void);
 
 #define MAX_IRQ  16
+#define INT_GATE_TYPE		0xE
+#define INT_PRESENT		0x80
+#define INT_GATE		INT_PRESENT | INT_GATE_TYPE	/* kesme kapisi */
+#define KERN_CODE_SEGMENT	0x8
+
 static int_handler_t irq_handlers[MAX_IRQ] = { NULL };
 
 /*
@@ -73,10 +78,26 @@ void irq_remove_handler(uint8_t irq_no) {
 }
 
 /*
- * irq_set_gates
+ * irq_set_gates, donanim kesmelerini idt tablosuna ekler.
  */
 void irq_set_gates(void){
- 
+	
+	idt_set_gate(32, _irq0, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(33, _irq1, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(34, _irq2, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(35, _irq3, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(36, _irq4, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(37, _irq5, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(38, _irq6, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(39, _irq7, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(40, _irq8, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(41, _irq9, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(42, _irq10, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(43, _irq11, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(44, _irq12, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(45, _irq13, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(46, _irq14, KERN_CODE_SEGMENT, INT_GATE);
+	idt_set_gate(47, _irq15, KERN_CODE_SEGMENT, INT_GATE); 
  
 }
 
@@ -89,11 +110,33 @@ void irq_remap(void){
 }
 
 /*
- * irq_handler
+ * irq_eoi
+ * 
+ * @param irq_num : irq numarasi
+ */
+void irq_eoi(uint8_t irq_num){
+
+	
+}
+
+/*
+ * irq_handler, irq isleyicisidir.
+ *
+ * @param regs : kaydediciler. hangi kaydedicileri icerdigini ogrenmek
+ *		 icin registers_t yapisini inceleyin.
  */
 void irq_handler(struct registers_t *regs){
 
-
+	int_handler_t handler;
+	
+	if(regs->int_num > 0x2F /* 47 */ && regs->int_num < 0x20 /* 32 */)
+		handler = NULL;
+	
+	handler = irq_handlers[regs->int_num - 0x20];
+	if(!handler)
+		irq_eoi(regs->int_num - 0x20);
+	handler(regs);
+	
 }
 
 /*
