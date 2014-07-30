7f51c36d8f43836daf36a93e7d3ac987f383d7f5
Burak Köken <colcsky@gmail.com>
2014-07-30 21:01:14 +0300

    Update regs.c

--- src/arch/i386/cpu/regs.c
+++ src/arch/i386/cpu/regs.c
@@ -29,28 +29,29 @@
  */
 void dump_regs(struct registers_t *regs){
  	
-	debug_print(KERN_EMERG, "Dump Registers\n"
-				"General purpose registers:\n"
-				"\teax = \033[1;37m%10P \033[0m ecx = \033[1;37m%10P\n"
-				"\t\033[0medx = \033[1;37m%10P \033[0m ebx = \033[1;37m%10P\n"
-				"\t\033[0mesp = \033[1;37m%10P \033[0m ebp = \033[1;37m%10P\n"
-				"\t\033[0mesi = \033[1;37m%10P \033[0m edi = \033[1;37m%10P\n"
-				"\033[0mSegment selectors:\n"
-				"\tds  = \033[1;37m%10P \033[0m es  = \033[1;37m%10P\n"
-				"\t\033[0mfs  = \033[1;37m%10P \033[0m gs  = \033[1;37m%10P\n"
-				"\t\033[0mcs  = \033[1;37m%10P \033[0m ss  = \033[1;37m%10P\n"
-				"\033[0mOther:\n"
-				"\t\033[0merror code  = \033[1;37m%10P\n"
-				"\t\033[0muser esp    = \033[1;37m%10P\n"
-				"\t\033[0meip         = \033[1;37m%10P\n"
-				"\t\033[0meflags      = \033[1;37m%10P",
-				regs->eax,regs->ecx,
-				regs->edx,regs->ebx,
-				regs->esp,regs->ebp,
-				regs->esi,regs->edi,
-				regs->ds,regs->es,regs->fs,regs->gs,regs->cs,regs->ss,
-				regs->err_code,regs->useresp,regs->eip,regs->eflags);
-  
+	debug_print(KERN_EMERG, "Dump Registers");
+	debug_print(KERN_DUMP,"General purpose registers:");
+	debug_print(KERN_DUMP,"\teax = \033[1;37m%10P \033[0m ecx = \033[1;37m%10P",regs->eax,
+										    regs->ecx);
+	debug_print(KERN_DUMP,"\t\033[0medx = \033[1;37m%10P \033[0m ebx = \033[1;37m%10P",regs->edx,
+											   regs->ebx);
+	debug_print(KERN_DUMP,"\t\033[0mesp = \033[1;37m%10P \033[0m ebp = \033[1;37m%10P",regs->esp,
+											   regs->ebp);			
+	debug_print(KERN_DUMP,"\t\033[0mesi = \033[1;37m%10P \033[0m edi = \033[1;37m%10P",regs->esi,
+											   regs->edi);
+	debug_print(KERN_DUMP,"\033[0mSegment selectors:");
+	debug_print(KERN_DUMP,"\tds  = \033[1;37m%10P \033[0m es  = \033[1;37m%10P",regs->ds,
+										    regs->es);				
+	debug_print(KERN_DUMP,"\t\033[0mfs  = \033[1;37m%10P \033[0m gs  = \033[1;37m%10P",regs->fs,
+										    	   regs->gs);				
+	debug_print(KERN_DUMP,"\t\033[0mcs  = \033[1;37m%10P \033[0m ss  = \033[1;37m%10P",regs->cs,
+										    	   regs->ss);
+	debug_print(KERN_DUMP,"\033[0mOther:");
+	debug_print(KERN_DUMP,"\t\033[0merror code  = \033[1;37m%10P",regs->err_code);
+	debug_print(KERN_DUMP,"\t\033[0muser esp    = \033[1;37m%10P",regs->useresp);
+	debug_print(KERN_DUMP,"\t\033[0meip         = \033[1;37m%10P",regs->eip);
+	debug_print(KERN_DUMP,"\t\033[0meflags      = \033[1;37m%10P",regs->eflags);
+
 }
 
 MODULE_AUTHOR("Burak Köken");
