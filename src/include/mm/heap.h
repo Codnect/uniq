0b8f47896af1f64dcdc5c339d12a8b69a0a5756b
Burak Köken <colcsky@gmail.com>
2014-08-04 22:35:47 +0300

    Create heap.h

---
+++ src/include/mm/heap.h
@@ -0,0 +1,31 @@
+/*
+ *  Copyright(C) 2014 Codnect Team
+ *  Copyright(C) 2014 Burak Köken
+ *
+ *  This file is part of Uniq.
+ *  
+ *  Uniq is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2 of the License.
+ *
+ *  Uniq is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UNIQ_HEAP_H__
+#define __UNIQ_HEAP_H__
+
+#include <uniq/types.h>
+
+uint32_t kmalloc_align(uint32_t size);
+uint32_t kmalloc_physic(uint32_t size,uint32_t *physic_addr);
+uint32_t kmalloc_aphysic(uint32_t size,uint32_t *physic_addr);
+uint32_t kmalloc(uint32_t size);
+void heap_init(void);
+
+#endif /* __UNIQ_HEAP_H__ */
