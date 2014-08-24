9230e5fbcd75f80faa4e72b51f7099716f172283
Burak Köken <colcsky@gmail.com>
2014-08-25 01:12:18 +0300

    Update heap.c

--- src/mm/heap.c
+++ src/mm/heap.c
@@ -238,6 +238,7 @@ static heap_blk_header_t *get_heap_blk_header(heap_blk_t *blk){
  */
 static void *blk_part_push(heap_blk_header_t *header,void *ptr){
 
+	assert(ptr && (uint32_t)header < (uint32_t)ptr);
 	uint32_t **addr = (uint32_t**)ptr;
 	*addr = (uint32_t*)header->point;
 	header->point = addr;
@@ -251,8 +252,7 @@ static void *blk_part_push(heap_blk_header_t *header,void *ptr){
  */
 static void *blk_part_pop(heap_blk_header_t *header){
 
-	assert(header && header->point != NULL);	
-
+	assert(header && header->point);	
 	void *addr = header->point;
 	uint32_t **point = header->point;
 	header->point = *point;
