25b076e95a4c7b69469ea799c570e265bdbc57a9
Burak Köken <colcsky@gmail.com>
2014-08-30 18:16:00 +0300

    Update task.h

--- src/include/uniq/task.h
+++ src/include/uniq/task.h
@@ -20,47 +20,7 @@
 #ifndef __UNIQ_TASK_H__
 #define __UNIQ_TASK_H__
 
-#include <uniq/types.h>
-
-typedef struct{
-	uint32_t present    : 1;   /* sayfa bellekte ise set edilir */
-   	uint32_t rw         : 1;   /* 0 ise sadece okuma, 1 ise okuma/yazma */
-   	uint32_t user       : 1;   /* 0 ise kernel, 1 ise user moddan da erisim olur */
-   	uint32_t accessed   : 1;   /* her erisimde set edilir */
-   	uint32_t dirty      : 1;   /* her yazmada set edilir */
-   	uint32_t reserved   : 7;   /* reserve edilmis */
-   	uint32_t frame      : 20;  /* frame adresi */
-}page_t;
-
-#define PAGE_MAX_LIMIT		1024	/* sayfa ve tablo maksimum girdi sayisi */
-#define PAGE_KERNEL_ACCESS	0x0
-#define PAGE_PRESENT		0x1
-/* user icin flaglar */
-#define PAGE_USER_ACCESS	0x1
-#define PAGE_RONLY		0x0
-#define PAGE_RWRITE		0x1
 
 
-typedef struct{
-	page_t pages[PAGE_MAX_LIMIT];
-}page_table_t;
-
-typedef struct{				
-
-	/* sayfa tablolari isaretci dizisi */
-	page_table_t *tables[PAGE_MAX_LIMIT];
-	/*
-	 * sayfa tablolarinin sayfa dizini girdisi halleri.
-	 * kisaca sayfa dizin tablosudur. gdt.c'yi incelediyseniz 
-	 * eger yabanci olmayacaksiniz. son 20 bit sayfa tablolarinin 
-	 * kacinci 4 KiB'lik adreste oldugunu belirtir.diger bitler
-	 * ise flaglardir.bu tablonun adresini cr3 kaydedicesine
-	 * atacagiz.
-	 */
-	uint32_t tables_physic[PAGE_MAX_LIMIT];
-	/* tables_physic adresi */
-	uint32_t physic_addr;
-	
-}page_dir_t;
 
 #endif /* __UNIQ_TASK_H__ */
