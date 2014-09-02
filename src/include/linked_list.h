c46c590d14075683e6b933153402363b617efcd2
Burak Köken <colcsky@gmail.com>
2014-09-02 14:35:38 +0300

    Update linked_list.h

--- src/include/linked_list.h
+++ src/include/linked_list.h
@@ -20,5 +20,26 @@
 #ifndef __UNIQ_LINKED_LIST_H__
 #define __UNIQ_LINKED_LIST_H__
 
+#include <uniq/types.h>
+#include <compiler.h>
+
+#define LINKED_LIST_SIGNATURE		0xEE384ACF
+
+typedef struct{
+	void *item;			/* item */
+	struct node_t *prev; 		/* onceki dugum */
+	struct node_t *next;		/* sonraki dugum */
+	void *link_list;		/* dugumun hangi listeye bagli oldugu */
+}__packed node_t;
+
+typedef struct{
+	uint32_t signature;		/* liste imzasi */
+	uint32_t size;			/* liste uzunlugu */
+	node_t *first_node;		/* ilk dugum */
+	node_t *last_node;		/* son dugum */
+}__packed linked_list_t;
+
+node_t *linked_list_push(linked_list_t *linked_list,void *item);
+
 
 #endif /* __UNIQ_LINKED_LIST_H__ */
