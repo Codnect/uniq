2a2fdede9d67d599cabf586ce1020fb21fe6c403
Burak Köken <colcsky@gmail.com>
2014-09-03 18:00:45 +0300

    Create syscall_nums.h

---
+++ src/include/uniq/syscall_nums.h
@@ -0,0 +1,24 @@
+/*
+ *  Copyright(C) 2014 Codnect Team
+ *  Copyright(C) 2014 Burak Köken
+ *
+ *  This file is part of Uniq.
+ *  
+ *  Uniq is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2 of the License.
+ *
+ *  Uniq is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UNIQ_SYSCALL_NUMS_H__
+#define __UNIQ_SYSCALL_NUMS_H__
+
+
+#endif /* __UNIQ_SYSCALL_NUMS_H__ */
