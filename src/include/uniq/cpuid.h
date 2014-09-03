ef041a397abd369a59e268c277d75a952b597a36
Burak Köken <colcsky@gmail.com>
2014-09-03 23:29:55 +0300

    Update cpuid.h

--- src/include/uniq/cpuid.h
+++ src/include/uniq/cpuid.h
@@ -23,19 +23,27 @@
 #include <uniq/types.h>
 
 typedef struct{
-    uint32_t eax,ebx,ecx,edx;
+	uint32_t eax,ebx,ecx,edx;
 }cpuid_regs_t;
 
 typedef struct{
-
+	char vendor_id[16];
+	uint8_t cpu_family;
 }cpuid_info_t;
 
+#define CPUID_VENDOR_ID			0x00000000
+
+#define AMD_VENDOR_NAME			"AuthenticAMD"
 #define AMD_SIGNATURE_EBX		0x68747541
 #define AMD_SIGNATURE_ECX		0x444d4163
 #define AMD_SIGNATURE_EDX		0x69746e65
 
+#define INTEL_VENDOR_NAME		"GenuineIntel"
 #define INTEL_SIGNATURE_EBX		0x756e6547
 #define INTEL_SIGNATURE_ECX		0x6c65746e
 #define INTEL_SIGNATURE_EDX		0x49656e69
+
+bool get_cpuid_info(cpuid_info_t *cpuid_info);
+void dump_cpuid_info(cpuid_info_t *cpuid_info);
 
 #endif /* __UNIQ_CPUID_H__ */
