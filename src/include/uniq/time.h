767d5087ba3f1176ecc679fd7f15f14ded9653d5
Burak Köken <colcsky@gmail.com>
2014-09-05 01:48:34 +0300

    Update time.h

--- src/include/uniq/time.h
+++ src/include/uniq/time.h
@@ -20,4 +20,23 @@
 #ifndef __UNIQ_TIME_H__
 #define __UNIQ_TIME_H__
 
+#include <uniq/types.h>
+
+typedef struct{
+	time_t tm_sec;		/* saniye */
+	time_t tm_min;		/* dakika */
+	time_t tm_hour;		/* saat */
+	time_t tm_mday;		/* ayin kacinci gunu */
+	time_t tm_mon;		/* ay */
+	time_t tm_year;		/* yil */
+	time_t tm_wday;		/* haftanin kacinci gunu */
+	time_t tm_yday;		/* yilin kacinci gunu */
+	time_t tm_isdst;	/* yaz saati uygulamasi */
+}tm_t;
+
+typedef struct{
+	time_t tv_sec;		/* saniye */
+	suseconds_t tv_usec;	/* mikrosaniye */
+}timeval_t;
+
 #endif /* __UNIQ_TIME_H__ */
