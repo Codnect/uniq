10543946d47e195317a4c2a162c96bf8057602d2
Burak Köken <colcsky@gmail.com>
2014-09-04 20:47:21 +0300

    Update utsname.h

--- src/include/uniq/utsname.h
+++ src/include/uniq/utsname.h
@@ -23,11 +23,11 @@
 #define UTSNAME_LENGTH		256
 
 typedef struct{
-	char  sysname[UTSNAME_LENGTH];
+	char sysname[UTSNAME_LENGTH];
 	char nodename[UTSNAME_LENGTH];
-	char  release[UTSNAME_LENGTH];
-	char  version[UTSNAME_LENGTH];
-	char  machine[UTSNAME_LENGTH];
+	char release[UTSNAME_LENGTH];
+	char version[UTSNAME_LENGTH];
+	char machine[UTSNAME_LENGTH];
 	char domainname[UTSNAME_LENGTH];
 }utsname_t;
 
