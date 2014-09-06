39e1e10ef38172abd9a4fa4901ab3a7b3d0a35a1
Burak Köken <colcsky@gmail.com>
2014-09-06 23:01:25 +0300

    Update types.h

--- src/include/uniq/types.h
+++ src/include/uniq/types.h
@@ -37,8 +37,8 @@ typedef signed int 		int32_t;
 typedef unsigned int 		uint32_t;
 
 /* ... */
-typedef long long int64_t;
-typedef unsigned long long uint64_t;
+typedef long long 		int64_t;
+typedef unsigned long long 	uint64_t;
 
 /*
  * mimari veri tipleri
