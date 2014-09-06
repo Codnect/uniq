939cd67834220944da3cd345f4d37104d950b3c2
Burak Köken <colcsky@gmail.com>
2014-09-07 00:52:42 +0300

    Create init.c

---
+++ src/init/init.c
@@ -0,0 +1,29 @@
+/*
+ *  Copyright(C) 2014 Codnect Team
+ *  Copyright(C) 2014 Burak Köken
+ *
+ *  This file is part of Nectlib.
+ *  
+ *  Nectlib is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2 of the License.
+ *
+ *  Nectlib is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <uniq/types.h>
+#include <uniq/module.h>
+
+void init_system(uint32_t argc,char *argv[]){
+
+
+}
+
+MODULE_AUTHOR("Burak Köken");
+MODULE_LICENSE("GNU GPL v2");
