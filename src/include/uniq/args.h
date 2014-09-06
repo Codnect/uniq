08f8d8d86106c3e91f010049fc6d105bb56fb967
Burak Köken <colcsky@gmail.com>
2014-09-07 00:09:22 +0300

    Create args.h

---
+++ src/include/uniq/args.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright(C) 2014 Codnect Team
+ *  Copyright(C) 2014 Burak Köken
+ *
+ *  This file is part of Uniq.
+ *  
+ *  Uniq is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2 of the License.
+ *
+ *  Uniq is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UNIQ_ARGS_H__
+#define __UNIQ_ARGS_H__
+
+typedef struct{
+	void *arg_value;
+	void *next_value;
+}args_value_t;
+
+typedef struct{
+	void *arg;
+	void *next_arg;
+	args_value_t *values;
+}args_map_t;
+
+#endif /* __UNIQ_ARGS__ */
