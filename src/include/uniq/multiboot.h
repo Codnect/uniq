c0c29762f3f4b0a1b7124bff5fa75e7e9f6838a6
Burak Köken <colcsky@gmail.com>
2014-09-07 17:30:31 +0300

    Update multiboot.h

--- src/include/uniq/multiboot.h
+++ src/include/uniq/multiboot.h
@@ -22,6 +22,19 @@
 
 #include <uniq/types.h>
 
+#define MULTIBOOT_LOADER_MAGIC    	0x2BADB002
+#define MULTIBOOT_FLAG_MEMORY     	0x001
+#define MULTIBOOT_FLAG_DEVICE  		0x002
+#define MULTIBOOT_FLAG_CMDLINE 		0x004
+#define MULTIBOOT_FLAG_MODULES    	0x008
+#define MULTIBOOT_FLAG_AOUT    		0x010
+#define MULTIBOOT_FLAG_ELF     		0x020
+#define MULTIBOOT_FLAG_MEMMAP    	0x040
+#define MULTIBOOT_FLAG_CONFIG  		0x080
+#define MULTIBOOT_FLAG_LOADER  		0x100
+#define MULTIBOOT_FLAG_APM     		0x200
+#define MULTIBOOT_FLAG_VBE     		0x400
+
 typedef struct{
 	uint32_t flags;
 	uint32_t mem_lower;
