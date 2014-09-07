98c7b7418cf09e991b9dee7c47973d827d58109e
Burak Köken <colcsky@gmail.com>
2014-09-07 17:09:36 +0300

    Update multiboot.h

--- src/include/uniq/multiboot.h
+++ src/include/uniq/multiboot.h
@@ -21,9 +21,8 @@
 #define __MULTIBOOT_H__
 
 #include <uniq/types.h>
-#include <compiler.h>
 
-struct mboot_t{
+typedef struct{
 	uint32_t flags;
 	uint32_t mem_lower;
 	uint32_t mem_upper;
@@ -48,13 +47,38 @@ struct mboot_t{
 	uint32_t vbe_interface_seg;
 	uint32_t vbe_interface_off;
 	uint32_t vbe_interface_len;
-}__packed;
+}__attribute__ ((packed)) mboot_info_t;
 
-struct mboot_memmap_t{
+typedef struct{
 	uint32_t size;
 	uint64_t base_addr;
 	uint64_t length;
 	uint32_t type;
-}__packed;
+}__attribute__ ((packed)) mboot_memmap_t;
+
+typedef struct{
+	uint16_t attributes;
+	uint8_t  winA, winB;
+	uint16_t granularity;
+	uint16_t winsize;
+	uint16_t segmentA, segmentB;
+	uint32_t realFctPtr;
+	uint16_t pitch;
+
+	uint16_t Xres, Yres;
+	uint8_t  Wchar, Ychar, planes, bpp, banks;
+	uint8_t  memory_model, bank_size, image_pages;
+	uint8_t  reserved0;
+
+	uint8_t  red_mask, red_position;
+	uint8_t  green_mask, green_position;
+	uint8_t  blue_mask, blue_position;
+	uint8_t  rsv_mask, rsv_position;
+	uint8_t  directcolor_attributes;
+	
+	uint32_t physbase;
+	uint32_t reserved1;
+	uint16_t reserved2;
+}__attribute__ ((packed)) vbe_info_t;
 
 #endif /* __MULTIBOOT_H__ */
