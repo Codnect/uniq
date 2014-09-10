ee38cd725321febc5eaab3b211ed1d0001b39e3d
Burak Köken <colcsky@gmail.com>
2014-09-10 11:51:10 +0300

    Create map.c

---
+++ src/libs/map.c
@@ -0,0 +1,25 @@
+/*
+ *  Copyright(C) 2014 Codnect Team
+ *  Copyright(C) 2014 Burak Köken
+ *
+ *  This file is part of Uniq.
+ *  
+ *  Uniq is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, version 2 of the License.
+ *
+ *  Uniq is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <uniq/module.h>
+#include <map.h>
+
+
+MODULE_AUTHOR("Burak Köken");
+MODULE_LICENSE("GNU GPL v2");
