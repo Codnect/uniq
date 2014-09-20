86ef2c1a7c747529a889f1997b37325312493514
Burak Köken <colcsky@gmail.com>
2014-09-20 19:29:46 +0300

    Update and rename hash_map.h to hashmap.h

--- src/include/hashmap.h
+++ src/include/hashmap.h
@@ -17,17 +17,32 @@
  *  with Uniq.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __UNIQ_HASH_MAP_H__
-#define __UNIQ_HASH_MAP_H__
+#ifndef __UNIQ_HASHMAP_H__
+#define __UNIQ_HASHMAP_H__
 
 #include <uniq/types.h>
 
-typedef struct _hash_map_entry_t{
+#define HASHMAP_SIGNATURE			0x79FFC571
 
-}hash_map_entry_t;
+typedef void (*hashmap_free_t)(void *x);
+typedef void *(*hashmap_dup_t)(void *x);
+typedef int32_t (*hashmap_cmp_t)(void *x,void *y);
+typedef uint32_t (*hashmap_hash_code_t)(void *key);
+
+typedef struct _hashmap_entry_t{
+	struct _hash_map_entry_t *next;
+	char *hash_key;
+	void *item;
+}hashmap_entry_t;
 
 typedef struct{
+	hashmap_hash_code_t hash_code;
+	uint32_t size;
+	uint32_t signature;
+	hashmap_entry_t **entries;
+	hashmap_cmp_t  hash_cmp;
+	hashmap_dup_t hash_dup;
+	hashmap_free_t hash_free;
+}hashmap_t;
 
-}hash_map_t;
-
-#endif /* __UNIQ_HASH_MAP_H__ */
+#endif /* __UNIQ_HASHMAP_H__ */
