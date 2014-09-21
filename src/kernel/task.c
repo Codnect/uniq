240ff3e76595df5d26f75d0db26fe3154df73e9b
Burak Köken <colcsky@gmail.com>
2014-09-21 23:32:31 +0300

    Update task.c

--- src/kernel/task.c
+++ src/kernel/task.c
@@ -18,7 +18,16 @@
  */
  
 #include <uniq/module.h>
+#include <uniq/task.h>
+#include <mm/mem.h>
+#include <mm/heap.h>
+#include <uniq/kernel.h>
+#include <string.h>
 
+uint32_t next_pid = 0;
+
+extern page_dir_t *kernel_dir;
+extern page_dir_t *current_dir;
 
 /*
  * goto_user_mode
@@ -29,12 +38,102 @@ void goto_user_mode(void){
 }
 
 /*
- * multitasking_init,
+ * get_pid,
  */
+uint32_t get_pid(void){
+
+	return 0;
+
+}
+
+/*
+ * page_table_clone,
+ *
+ * @param src_table :
+ * @param physical_addr :
+ */
+page_table_t *page_table_clone(page_table_t *src_table,uint32_t *physical_addr){
+
+	page_table_t *clone = (page_table_t*)kmalloc_aphysic(sizeof(page_table_t),physical_addr);
+	memset(clone,0,sizeof(page_table_t));
+
+	for(uint32_t i = 0;i < 1024;i++){
+
+
+	}
+
+	return clone;
+
+}
+
+/*
+ * page_directory_clone,
+ *
+ * @param dir :
+ */
+page_dir_t *page_directory_clone(page_dir_t *src_directory){
+
+	uint32_t physical;
+	page_dir_t *clone = (page_dir_t*)kmalloc_aphysic(sizeof(page_dir_t),&physical);
+	memset(clone,0,sizeof(page_dir_t));
+	clone->physical_addr = physical;
+
+	for(uint32_t i = 0; i < PAGE_MAX_LIMIT;i++){
+
+		if(!src_directory->tables[i] || (uint32_t)src_directory->tables[i] == MAX_LIMIT)
+			continue;
+
+		if(kernel_dir->tables[i] == src_directory->tables[i]){
+
+			clone->tables[i] = src_directory->tables[i];
+			clone->physical_tables[i] = src_directory->physical_tables[i];
+
+		}
+		else{
+
+			uint32_t physic;
+            		clone->tables[i] = page_table_clone(src_directory->tables[i],&physic);
+            		clone->physical_tables[i] = physic | 0x7;
+
+		}
+
+	}
+
+}
+
+/*
+ * page_directory_free,
+ *
+ * @param directory :
+ */
+void *page_directory_free(page_dir_t *directory){
+
+
+}
+
+/*
+ * switch_task,
+ */
+void switch_task(void){
+
+
+}
+
+/*
+ * switch_next_task,
+ */
+void switch_next_task(void){
+
+
+}
+
+/*
+* multitasking_init,
+*/
 void multitasking_init(void){
  
  
 }
-
+	
 MODULE_AUTHOR("Burak Köken");
 MODULE_LICENSE("GNU GPL v2");
